6c996654c0d9537da54cd643c39ec9b4fbc40374
TurgunovDostonbek <turgunovdostonbek@gmail.com>
2024-05-22 00:19:24 +0500

    1816. Truncate Sentence

--- 3-Misol/index.js
+++ 3-Misol/index.js
@@ -17,12 +17,9 @@
 
 
 ////////////////////////////////////   ////////////////////////////
-             //   Javbolar //
-
-            var truncateSentence = function(s, k) {
-                let a = s.split(' ').slice(0,k).join(" ")
-                return a
-            };  
-            
-            
-            console.log(truncateSentence("Hello how are you Contestant", k = 4));
+         ////   Javbolar ////////
+        var truncateSentence = function(s, k) {
+            let a = s.split(' ').slice(0,k).join(" ")
+            return a
+        };  
+        console.log(truncateSentence("Hello how are you Contestant", k = 4));
