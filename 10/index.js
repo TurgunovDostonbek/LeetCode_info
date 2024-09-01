399e7ac3a1ccebf69107b7f64f5ea25259912814
TurgunovDostonbek <turgunovdostonbek@gmail.com>
2024-09-01 21:48:23 +0500

    2469. Convert the Temperature

---
+++ 10/index.js
@@ -0,0 +1,28 @@
+// Sizga Selsiy bo'yicha ikki kasrli kasrga yaxlitlangan manfiy bo'lmagan suzuvchi nuqta berilgan, bu Selsiy bo'yicha haroratni bildiradi.
+// Siz Selsiyni Kelvin va Farengeytga aylantirib, uni ans = [kelvin, farengeyt] massivi sifatida qaytarishingiz kerak.
+// ans massivini qaytaring. Haqiqiy javobdan 10-5 gacha bo'lgan javoblar qabul qilinadi.
+// Shu esta tutilsinki:
+
+// Kelvin = Selsiy + 273,15
+// Farengeyt = Selsiy * 1,80 + 32,00
+
+// Example 1:
+
+// Input: celsius = 36.50
+// Output: [309.65000,97.70000]
+// Explanation: Temperature at 36.50 Celsius converted in Kelvin is 309.65 and converted in Fahrenheit is 97.70.
+// Example 2:
+
+// Input: celsius = 122.11
+// Output: [395.26000,251.79800]
+// Explanation: Temperature at 122.11 Celsius converted in Kelvin is 395.26 and converted in Fahrenheit is 251.798.
+
+// var convertTemperature = function (celsius) {
+//   const a = celsius + 273.15;
+//   const b = celsius * 1.8 + 32.0;
+//   console.log([a, b]);
+//   return [a, b];
+// };
+
+// const res = convertTemperature(36.5);
+// console.log(res);
