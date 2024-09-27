3070aad49c9bebd28882412ce4a91697790aad21
TurgunovDostonbek <turgunovdostonbek@gmail.com>
2024-09-27 17:13:34 +0500

    268. Missing Number

--- 1-Misol/js.js
+++ 1-Misol/js.js
@@ -1,4 +1,4 @@
-//  Oraliqda aniq raqamlarni numso'z ichiga olgan massiv berilgan bo'lsa , qatorda etishmayotgan yagona raqamni qaytaring .n[0, n]..
+//  Oraliqda aniq raqamlarni num so'z ichiga olgan massiv berilgan bo'lsa , qatorda etishmayotgan yagona raqamni qaytaring .n[0, n]..
 
 // 1-misol:
 
@@ -12,18 +12,15 @@
 //  Chiqish: 8
 //  Izoh: n = 9 chunki 9 ta raqam mavjud, shuning uchun barcha raqamlar [0,9] oralig'ida. 8 diapazondagi etishmayotgan raqam, chunki u raqamlarda ko'rinmaydi.
 
-
-
 ////////////////////////////////////   ////////////////////////////
-             //   Javbolar //
+//   Javbolar //
 
-        var missingNumber = function(nums) {
-           const n = nums.length;   ///  birinchi nums uzunligi topiladi 
-           const Tsum = (n * (n + 1)) / 2;  /// (3 * (3 + 1 ) / 2) = 6
-           const actualSum = nums.reduce((acc, num) => acc + num, 0);  /// hammasini yig"amiz 4 
-           console.log(Tsum - actualSum); //  6 - 4 = 2 
-        };
+var missingNumber = function (nums) {
+  const n = nums.length; ///  birinchi nums uzunligi topiladi
+  const Tsum = (n * (n + 1)) / 2; /// (3 * (3 + 1 ) / 2) = 6
+  const actualSum = nums.reduce((acc, num) => acc + num, 0); /// hammasini yig"amiz 4
+  console.log(Tsum - actualSum); //  6 - 4 = 2
+};
 
-        missingNumber([3,0,1])
-        missingNumber([9,6,4,2,3,5,7,0,1])
-
+missingNumber([3, 0, 1]);
+missingNumber([9, 6, 4, 2, 3, 5, 7, 0, 1]);
